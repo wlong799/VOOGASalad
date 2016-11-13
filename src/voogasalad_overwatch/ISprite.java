dd26965a0a996fcbde2022d00a4e96d369998dae
Michael Schroeder <michael.schroeder@duke.edu>
2016-11-13 17:35:14 -0500

    Update ISprite.java

--- src/voogasalad_overwatch/ISprite.java
+++ src/voogasalad_overwatch/ISprite.java
@@ -6,8 +6,8 @@ public interface ISprite {
     void setPosition(Position pos);
     // get this sprite's position
     Position getPosition();
-    // kill this sprite
-    void kill();
+    // set this sprite's isDead parameter
+    void setDead(boolean dead);
     // check if it is dead
     boolean isDead();
     // what to do when this sprite collides with another sprite
