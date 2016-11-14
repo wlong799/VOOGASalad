104bed87a516a27a2550349feb3ecf01e29f5d4f
Jay Wang <zw48@duke.edu>
2016-11-14 18:23:56 -0500

    Modify ISprite

--- src/game_object/ISprite.java
+++ src/game_object/ISprite.java
@@ -1,6 +1,5 @@
 package game_object;
 
-import javafx.scene.image.ImageView;
 import voogasalad_overwatch.Collision;
 
 import java.util.*;
@@ -9,17 +8,19 @@ public interface ISprite {
     
     // set this sprites position
     void setPosition(Position pos);
+    
     // get this sprite's position
     Position getPosition();
+    
     // set this sprite's isDead parameter
     void setDead(boolean dead);
+    
     // check if it is dead
     boolean isDead();
+    
     // what to do when this sprite collides with another sprite
-    void collided(ISprite otherSprite);
-    //sets the new hp of sprite
-    void setHP(double hp);
-    double getHP();
+    void collided(CollisionBody otherBody);
+    
     //sets the new collision property of sprite
     void setCollisionProperty(Collision coll);
     
