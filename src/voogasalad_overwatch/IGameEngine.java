c34b2cb1d3ee1d6edcb168337da782467c7c127b
Rachel Bransom <rnb11@duke.edu>
2016-11-13 20:42:28 -0500

    merge conflicts

--- src/voogasalad_overwatch/IGameEngine.java
+++ src/voogasalad_overwatch/IGameEngine.java
@@ -1,5 +1,11 @@
 package voogasalad_overwatch;
 
 public interface IGameEngine {
-
+	void setCurrentLevel(ILevel level);
+	
+	void setPhysicsEngine(IPhysicsEngine physicsEngine);
+	
+	void setControlHandler(IControlHandler handler);
+	
+	void start();
 }
