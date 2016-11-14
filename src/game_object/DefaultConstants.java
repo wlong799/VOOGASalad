095af5fa6d36007f8f623ec1499d7a23cad9a61d
Jay Wang <zw48@duke.edu>
2016-11-14 18:24:52 -0500

    Create DefaultConstants.java for all the constants

---
+++ src/game_object/DefaultConstants.java
@@ -0,0 +1,10 @@
+package game_object;
+
+public class DefaultConstants {
+	public static final double MOVING_UNIT = 5;
+	public static final double JUMPING_UNIT = 30;
+	
+	public static final int HERO_CATEGORY_BIT_MASK = 1 << 0;
+	public static final int ENEMY_CATEGORY_BIT_MASK = 1 << 1;
+	
+}
