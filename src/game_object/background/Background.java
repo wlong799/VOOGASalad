4d0782f9ee6278dc920a0fe93f5390aede9d50fb
Yilun Zhou <yz192@duke.edu>
2016-11-15 17:48:49 -0500

    Add background class

---
+++ src/game_object/background/Background.java
@@ -0,0 +1,14 @@
+package game_object.background;
+
+import java.util.*;
+
+/**
+ * This represents the background scene of the game
+ * @author Yilun
+ *
+ */
+public class Background {
+
+	private ArrayList<String> imgPaths;
+	
+}
