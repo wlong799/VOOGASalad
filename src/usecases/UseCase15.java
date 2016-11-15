3fb27871ea4c2a3bbcdd02b2ccc893fe725e4ebe
Jay Wang <zw48@duke.edu>
2016-11-15 16:04:35 -0500

    Clean up old classes

--- src/usecases/UseCase15.java
+++ src/usecases/UseCase15.java
@@ -1,20 +1,14 @@
 package usecases;
 
-import com.thoughtworks.xstream.XStream;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import voogasalad_overwatch.Game;
-import voogasalad_overwatch.Sprite;
-
 public class UseCase15 {
     public static void main(String[] args){
-        Game game = new Game();
-        Sprite sprite = new Sprite();
-        new EventHandler<ActionEvent>(){
-            @Override
-            public void handle (ActionEvent event) {
-                game.editSprite(sprite);
-            }
-        };
+//        Game game = new Game();
+//        Sprite sprite = new Sprite();
+//        new EventHandler<ActionEvent>(){
+//            @Override
+//            public void handle (ActionEvent event) {
+//                game.editSprite(sprite);
+//            }
+//        };
     }
 }
--- src/usecases/UseCase20.java
+++ src/usecases/UseCase20.java
@@ -1,23 +1,16 @@
 package usecases;
 
-import com.thoughtworks.xstream.XStream;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import voogasalad_overwatch.AuthorEnvironment;
-import voogasalad_overwatch.Game;
-import voogasalad_overwatch.Sprite;
-
 public class UseCase20 {
     public static void main(String[] args){
-        Game game = new Game();
-        Sprite sprite = new Sprite();
-        AuthorEnvironment ae = new AuthorEnvironment();
-        ae.load();
-        new EventHandler<ActionEvent>(){
-            @Override
-            public void handle (ActionEvent event) {
-                game.editSprite(sprite);
-            }
-        };
+//        Game game = new Game();
+//        Sprite sprite = new Sprite();
+//        AuthorEnvironment ae = new AuthorEnvironment();
+//        ae.load();
+//        new EventHandler<ActionEvent>(){
+//            @Override
+//            public void handle (ActionEvent event) {
+//                game.editSprite(sprite);
+//            }
+//        };
     }
 }
