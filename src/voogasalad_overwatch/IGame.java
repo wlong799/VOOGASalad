83009a4de0d30279778569444277091ae76e9636
Rachel Bransom <rnb11@duke.edu>
2016-11-16 12:36:32 -0500

    minor design document decisions

--- src/voogasalad_overwatch/IGame.java
+++ src/voogasalad_overwatch/IGame.java
@@ -2,13 +2,39 @@ package voogasalad_overwatch;
 
 import java.util.Collection;
 
+/**
+ * @author 
+ *contains everything that we need for a GameEngine 
+ *to run. It contains several Level objects, 
+ *and each Level object has some level specific data, 
+ *for example sprites and goals.
+
+ */
 public interface IGame {
 	
+	/**
+	 * sets the levels
+	 * @param index
+	 * @param level
+	 */
 	void setLevel(int index, ILevel level);
 	
+	/**
+	 * @param index
+	 * @return level corresponding to the index
+	 */
 	ILevel getLevel(int index);
 	
+	/**
+	 * sets which sprites should be created at the 
+	 * start of the program
+	 * @param starts
+	 */
 	void setStartSprites(Collection<ISprite> starts);
 	
+	/**
+	 * sets which sprites determine the end of a program
+	 * @param ends
+	 */
 	void setEndSprites(Collection<ISprite> ends);
 }
