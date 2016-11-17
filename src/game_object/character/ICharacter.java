c6f191da3b123a106898e6cff7f6d1388bf5ca34
Jay Wang <zw48@duke.edu>
2016-11-16 19:16:13 -0500

    Create ICharacter for all characters

---
+++ src/game_object/character/ICharacter.java
@@ -0,0 +1,25 @@
+package game_object.character;
+
+import game_object.simulation.ICollisionBody;
+import game_object.simulation.IPhysicsBody;
+import game_object.weapon.Weapon;
+
+/**
+ * Character interface
+ * @author Jay
+ */
+public interface ICharacter extends ICollisionBody, IPhysicsBody {
+
+	void setMaxHP(int maxHP);
+	
+	double getMaxHP();
+	
+	void setCurrentHP(double currentHP);
+	
+	double getCurrentHP();
+	
+	Weapon getCurrentWeapon();
+
+	void setCurrentWeapon(Weapon currentWeapon);
+	
+}
