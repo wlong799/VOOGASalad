407baf88114c218d0cd829b4c8de704b91941e7e
Charles Xu <cx15@duke.edu>
2016-11-18 21:55:41 -0500

    no magic numbers

--- src/network/integration_tests/ServerClientTest.java
+++ src/network/integration_tests/ServerClientTest.java
@@ -24,20 +24,23 @@ public class ServerClientTest {
 	public static final String SOME_MSG = "some random massage";
 	public static final String SOME_DIFF_MSG = "another random massage";
 	
+	public static final int PORT = 9999;
+	public static final int DELAY_MILLIS = 1000;
+	
 	public static void main(String[] args) {
 		try {
-			Coordinator cor = new Coordinator(9999);
+			Coordinator cor = new Coordinator(PORT);
 			NetworkClient c1 = new NetworkClient();
 			NetworkClient c2 = new NetworkClient();
 			NetworkClient c3 = new NetworkClient();
 			c1.broadcast(new Message(SOME_MSG));
-			Thread.sleep(1000);
+			Thread.sleep(DELAY_MILLIS);
 			System.out.println(c2.read().peek().toString());
 			System.out.println(c2.read().isEmpty());
 			System.out.println(c3.read().peek().toString());
 			System.out.println(c3.read().isEmpty());
 			c1.broadcast(new Message(SOME_DIFF_MSG));
-			Thread.sleep(1000);
+			Thread.sleep(DELAY_MILLIS);
 			System.out.println(c2.read().peek().toString());
 			cor.shutdown();
 		} catch (IOException | ServerDownException | InterruptedException e) {
