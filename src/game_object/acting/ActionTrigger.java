43b285e956946128fa3788128d63d2b237513cb3
Jay Wang <zw48@duke.edu>
2016-11-20 15:47:08 -0500

    Add accessors to trigger

--- src/game_object/acting/ActionTrigger.java
+++ src/game_object/acting/ActionTrigger.java
@@ -1,17 +1,42 @@
 package game_object.acting;
 
+import game_object.core.ExceptionThrower;
 import game_object.core.ISprite;
 
+/**
+ * An action trigger that associates an event with a sprite's action.
+ * @author Yilun, Jay
+ *
+ */
 public class ActionTrigger {
 
-	ISprite mySprite;
-	Event myEvent;
-	ActionName myActionName;
+	private Event myEvent;
+	private final ISprite mySprite;
+	private final ActionName myActionName;
 	
-	public ActionTrigger(ISprite sprite, Event event, ActionName actionName) {
-		mySprite = sprite;
+	public ActionTrigger(Event event, ISprite sprite, ActionName actionName) {
+		if (event == null || sprite == null || actionName == null) {
+			ExceptionThrower.illegalArgs("event, sprite, and actionName cannot be null");
+		}
 		myEvent = event;
+		mySprite = sprite;
 		myActionName = actionName;
+	}
+	
+	public void setEvent(Event event) {
+		myEvent = event;
+	}
+	
+	public Event getEvent() {
+		return myEvent;
+	}
+	
+	public ISprite getSprite() {
+		return mySprite;
+	}
+	
+	public ActionName getActionName() {
+		return myActionName;
 	}
 	
 }
