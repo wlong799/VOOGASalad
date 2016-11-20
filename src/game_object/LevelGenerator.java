00124bde6c016f478b2d51617cdebe449512d298
Yilun Zhou <yz192@duke.edu>
2016-11-20 09:54:25 -0500

    Add image style to test levels

--- src/game_object/LevelGenerator.java
+++ src/game_object/LevelGenerator.java
@@ -9,6 +9,7 @@ import game_object.block.StaticBlock;
 import game_object.character.Enemy;
 import game_object.character.Hero;
 import game_object.core.Dimension;
+import game_object.core.ImageStyle;
 import game_object.core.Position;
 import game_object.core.Velocity;
 import game_object.level.Level;
@@ -38,10 +39,16 @@ public class LevelGenerator {
 		Level level = new Level();
 		Hero hero = new Hero(new Position(30, 30), new Dimension(40, 60), heroImages);
 		hero.setVelocity(new Velocity(50,0));
+		hero.setImageStyle(ImageStyle.FIT);
+		
 		Enemy enemy = new Enemy(new Position(300,400),new Dimension(40, 60), enemyImages);
+		enemy.setImageStyle(ImageStyle.FIT);
 		
 		StaticBlock smackDown = new StaticBlock(new Position(340, 50), new Dimension(100, 200), blockImages);
+		smackDown.setImageStyle(ImageStyle.TILE);
+		
 		StaticBlock ground = new StaticBlock(new Position(0, 500), new Dimension(2000, 500), blockImages);
+		ground.setImageStyle(ImageStyle.TILE);
 		
 		level.addHero(hero);
 		level.addEnemy(enemy);
@@ -73,7 +80,10 @@ public class LevelGenerator {
 		Level level = new Level();
 		Hero hero = new Hero(new Position(30, 30), new Dimension(40, 60), heroImages);
 		hero.setVelocity(new Velocity(50, 0));
+		hero.setImageStyle(ImageStyle.FIT);
+		
 		StaticBlock ground = new StaticBlock(new Position(0, 500), new Dimension(2000, 500), blockImages);
+		ground.setImageStyle(ImageStyle.TILE);
 		
 		level.addHero(hero);
 		level.addStaticBlock(ground);
