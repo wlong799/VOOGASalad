398506c6075268f7c94ee351f38652bdd4b67cf0
Will Long <wlong799@gmail.com>
2016-11-21 18:46:51 -0500

    Edited appearance of ComponentView

--- src/authoring/view/components/ComponentView.java
+++ src/authoring/view/components/ComponentView.java
@@ -5,18 +5,29 @@ import authoring.view.AbstractView;
 import authoring.constants.UIConstants;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
+/**
+ * ComponentView displays the important information from Component, including the template sprite's image, the title of
+ * the component, and its description.
+ */
 public class ComponentView extends AbstractView {
+    private static final double INNER_PADDING = 5;
+    private static final double OUTER_PADDING = 10;
+    private static final double TITLE_FONT_SIZE = 20;
+    private static final double DESCRIPTION_FONT_SIZE = 12;
 
-    private ImageView imageView;
-    private VBox box;
+    private HBox myContentBox;
+    private ImageView myTemplateSpriteImageView;
+    private Text myTitleText, myDescriptionText;
     private Component myComponent;
 
     public ComponentView(AuthoringController controller) {
@@ -25,45 +36,49 @@ public class ComponentView extends AbstractView {
 
     public void setComponent(Component component) {
         myComponent = component;
-        updateUI();
+        updateLayoutSelf();
     }
 
     @Override
     protected void updateLayoutSelf() {
+        myTemplateSpriteImageView.setImage(new Image(myComponent.getImagePath()));
+        myTitleText.setText(myComponent.getTitle());
+        myDescriptionText.setText(myComponent.getDescription());
     }
 
     @Override
     protected void initUI() {
-        imageView = new ImageView();
-        box = new VBox();
-        box.setAlignment(Pos.CENTER);
-        box.getChildren().addAll(imageView);
-        box.setPrefWidth(this.getWidth());
-        box.setPrefHeight(this.getHeight());
-        box.setAlignment(Pos.CENTER);
-        box.setSpacing(10);
-        box.setPadding(new Insets(5, 5, 5, 5));
-        imageView.setPreserveRatio(true);
-        imageView.setFitWidth(
+        VBox vBox = initializeTextBoxes();
+        myTemplateSpriteImageView = new ImageView();
+        myContentBox = new HBox(INNER_PADDING, myTemplateSpriteImageView, vBox);
+        myContentBox.setAlignment(Pos.CENTER);
+        myContentBox.setPrefWidth(getWidth());
+        myContentBox.setPrefHeight(getHeight());
+        myContentBox.setPadding(new Insets(OUTER_PADDING));
+        myTemplateSpriteImageView.setPreserveRatio(true);
+        myTemplateSpriteImageView.setFitWidth(
                 UIConstants.COMPONENT_WIDTH * UIConstants.COMPONENT_WIDTH_PORTION);
-        this.addUI(box);
+        this.addUI(myContentBox);
         setOnDrag();
     }
 
-    private void updateUI() {
-        imageView.setImage(new Image(myComponent.getImagePath()));
-        Tooltip tip = new Tooltip(myComponent.getTitle());
-        Tooltip.install(imageView, tip);
+    private VBox initializeTextBoxes() {
+        myTitleText = new Text();
+        myTitleText.setFont(new Font(20));
+        myDescriptionText = new Text();
+        myDescriptionText.setFont(new Font(12));
+        VBox vBox = new VBox(INNER_PADDING, myTitleText, myDescriptionText);
+        return vBox;
     }
 
     private void setOnDrag() {
         AuthoringController controller = getController();
-        imageView.setOnDragDetected(event -> {
+        getUI().setOnDragDetected(event -> {
             controller.getComponentController().setCurrentlyCopiedSprite(myComponent.copySpriteFromTemplate());
 
-            Dragboard db = imageView.startDragAndDrop(TransferMode.COPY);
+            Dragboard db = myTemplateSpriteImageView.startDragAndDrop(TransferMode.COPY);
             ClipboardContent content = new ClipboardContent();
-            content.putImage(imageView.getImage());
+            content.putImage(myTemplateSpriteImageView.getImage());
             db.setContent(content);
             event.consume();
         });
