14679053da2b49bb7dab45a07e56534243faac16
ms548 <ms548@duke.edu>
2016-11-20 21:45:04 -0500

    make animation much more efficient

--- src/game_object/Main.java
+++ src/game_object/Main.java
@@ -1,7 +1,9 @@
 package game_object;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import game_engine.GameEngine;
 import game_object.level.Level;
 import game_object.visualization.ISpriteVisualization;
@@ -37,28 +39,35 @@ public class Main extends Application {
         Scene scene = new Scene(g, 600, 600, Color.WHITE);
         s.setScene(scene);
         s.show();
-        
+        Map<ISpriteVisualization, ImageView> spriteViewMap =
+                new HashMap<ISpriteVisualization, ImageView>();
+        for (ISpriteVisualization sp : l.getAllSpriteVisualizations()) {
+            ImageView image =
+                    new ImageView(new File(sp.getImagePath()).toURI().toString());// ,
+            image.setX(sp.getXForVisualization());
+            image.setY(sp.getYForVisualization());
+
+            image.setFitWidth(sp.getWidthForVisualization());
+            image.setFitHeight(sp.getHeightForVisualization());
+            spriteViewMap.put(sp, image);
+            g.getChildren().add(image);
+        }
         KeyFrame frame = new KeyFrame(Duration.millis(1000.0 / 60.0),
                                       new EventHandler<ActionEvent>() {
                                           @Override
                                           public void handle (ActionEvent event) {
-                                              object.update(5.0/ 60.0);
+                                              object.update(5.0 / 60.0);
                                               List<ISpriteVisualization> sprites =
                                                       l.getAllSpriteVisualizations();
-                                              g.getChildren().clear();
-                                              for (ISpriteVisualization s : sprites) {
-                                                  //System.out.println(s.getImagePath());
-                                                  
-                                                  //Image im = new Image(new File(System.getProperty("user.dir")+s.getImagePath()).toURI().toString());
-                                                  ImageView image =
-                                                          new ImageView(new File(s.getImagePath()).toURI().toString());//,
-                                                  image.setX(s.getXForVisualization());
-                                                  image.setY(s.getYForVisualization());
-                                                  
-                                                  image.setFitWidth(s.getWidthForVisualization());
-                                                  image.setFitHeight(s.getHeightForVisualization());
-                                                  
-                                                  g.getChildren().add(image);
+                                              // g.getChildren().clear();
+                                              for (ISpriteVisualization sprite : spriteViewMap
+                                                      .keySet()) {
+                                                  // System.out.println(s.getImagePath());
+
+                                                  // Image im = new Image(new
+                                                  // File(System.getProperty("user.dir")+s.getImagePath()).toURI().toString());
+                                                  spriteViewMap.get(sprite).setX(sprite.getXForVisualization());
+                                                  spriteViewMap.get(sprite).setY(sprite.getYForVisualization());
                                               }
                                           }
                                       });
