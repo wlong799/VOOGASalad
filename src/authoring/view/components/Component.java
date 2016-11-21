1b0be0bebacb2fb3b43561c3311807ba8e7e1be1
Will Long <wlong799@gmail.com>
2016-11-21 17:20:26 -0500

    Edited component class to use template sprites
    Component class now holds actual template sprites rather than just a
    game object type and an image. This will allow for more customization
    down the line. Still needs to actually allow templates to be edited.

--- src/authoring/view/components/Component.java
+++ src/authoring/view/components/Component.java
@@ -1,29 +1,78 @@
 package authoring.view.components;
 
 import game_object.GameObjectType;
+import game_object.block.StaticBlock;
+import game_object.character.Enemy;
+import game_object.character.Hero;
+import game_object.core.Dimension;
+import game_object.core.ISprite;
+import game_object.core.Position;
 
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Component class provides a way for users to have editable game component templates to add to the environment. Each
+ * component has a title and a description, and holds a single template sprite, which can be edited. Editing the template
+ * edits all currently linked sprites on screen created using the template.
+ */
+// TODO: 11/21/16 Make a way to edit the template sprite
+// TODO: 11/21/16 Make a way to link all previously created sprites and update them as well 
 public class Component {
-	
-	private GameObjectType myType;
-	private String myTitle;
-	private String myImagePath;
-	
-	public Component(GameObjectType type, String title, String imagePath) {
-		myType = type;
-		myTitle = title;
-		myImagePath = imagePath;
-	}
-	
-	public GameObjectType getType() {
-		return myType;
-	}
-	
-	public String getTitle() {
-		return myTitle;
-	}
-	
-	public String getImagePath() {
-		return myImagePath;
-	}
-	
+    private static final String COPY_ERROR = "Error when copying from template sprite.";
+
+    private ISprite myTemplateSprite;
+    private String myTitle;
+    private String myDescription;
+
+    public Component(GameObjectType gameObjectType, String imagePath, String title, String description) {
+        myTemplateSprite = createTemplateSpriteFromType(gameObjectType, imagePath);
+        myTitle = title;
+        myDescription = description;
+    }
+
+    public ISprite getSprite() {
+        ISprite sprite = null;
+        try {
+            Constructor<? extends ISprite> spriteConstructor = myTemplateSprite.getClass().getConstructor(
+                    Position.class, Dimension.class, String.class);
+            sprite = spriteConstructor.newInstance(myTemplateSprite.getPosition(), myTemplateSprite.getDimension(),
+                    myTemplateSprite.getImagePaths());
+        } catch (Exception e) {
+            System.out.println(COPY_ERROR);
+        }
+        return sprite;
+    }
+
+    public String getTitle() {
+        return myTitle;
+    }
+
+    public String getDescription() {
+        return myDescription;
+    }
+
+    public String getImagePath() {
+        return myTemplateSprite.getImagePath();
+    }
+
+    private ISprite createTemplateSpriteFromType(GameObjectType gameObjectType, String imagePath) {
+        ISprite sprite = null;
+        List<String> path = new ArrayList<>();
+        path.add(imagePath);
+        switch (gameObjectType) {
+            case Enemy:
+                sprite = new Enemy(new Position(0, 0), new Dimension(0, 0), path);
+                break;
+            case Hero:
+                sprite = new Hero(new Position(0, 0), new Dimension(0, 0), path);
+                break;
+            case StaticBlock:
+                sprite = new StaticBlock(new Position(0, 0), new Dimension(0, 0), path);
+                break;
+        }
+        return sprite;
+    }
 }
