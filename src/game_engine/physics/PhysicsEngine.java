79ec15759d812bda85fe2c63692e063851ee8ee7
Bill Yu <han.yu@duke.edu>
2016-11-30 22:02:03 -0500

    fix gravity update

--- src/game_engine/physics/PhysicsEngine.java
+++ src/game_engine/physics/PhysicsEngine.java
@@ -35,6 +35,7 @@ public class PhysicsEngine extends AbstractPhysicsEngine {
 
 	@Override
 	public double calculateNewVerticalVelocity(IPhysicsBody body, double elapsedTime) {
+		System.out.println(myLevel.getPhysicsParameters().getGravity());
 		double vy = body.getVelocity().getYVelocity();
 		double newvy = vy + elapsedTime * myLevel.getPhysicsParameters().getGravity();
 		if (Math.abs(newvy) > myLevel.getPhysicsParameters().getMaxThreshold()) {
