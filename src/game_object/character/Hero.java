3dd2d1609adbf3a134a418d4f8cbf7276d9c52f0
Jay Wang <zw48@duke.edu>
2016-12-03 15:28:35 -0500

    Revert the accidentally deleted part in levelgenerator when merging

--- src/game_object/character/Hero.java
+++ src/game_object/character/Hero.java
@@ -1,9 +1,7 @@
 package game_object.character;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import game_object.acting.ActionTrigger;
 import game_object.constants.DefaultConstants;
 import game_object.core.Dimension;
 import game_object.core.ExceptionThrower;
@@ -11,7 +9,7 @@ import game_object.core.Position;
 import game_object.simulation.ICollisionBody;
 import game_object.weapon.Weapon;
 
-public class Hero extends AbstractCharacter implements IUpgrader, ITriggerable {
+public class Hero extends AbstractCharacter implements IUpgrader {
 
 	private boolean myHasProjectile = false;
 	
@@ -23,17 +21,6 @@ public class Hero extends AbstractCharacter implements IUpgrader, ITriggerable {
 	
 	@Override
 	public void shoot() {
-		ExceptionThrower.notYetSupported();
-	}
-	
-	@Override
-	public ArrayList<ActionTrigger> getActionTriggers() {
-		ExceptionThrower.notYetSupported();
-		return null;
-	}
-
-	@Override
-	public void setActionTriggers(ArrayList<ActionTrigger> ats) {
 		ExceptionThrower.notYetSupported();
 	}
 
