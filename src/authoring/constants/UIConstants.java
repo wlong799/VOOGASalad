8db18f2f1683ef5920d22e2765694304d2d12488
Rachel Bransom <rnb11@duke.edu>
2016-12-07 20:17:26 -0500

    created english text resource file

--- src/authoring/constants/UIConstants.java
+++ src/authoring/constants/UIConstants.java
@@ -2,16 +2,9 @@ package authoring.constants;
 
 public class UIConstants {
 	
-	public static final double CANVAS_STARTING_WIDTH = 1500;
-	public static final double CANVAS_STARTING_HEIGHT = 800;
-	public static final double TOP_HEIGHT = 30;
-	public static final double RIGHT_WIDTH = 200;
-	
-	//expand/shrink buttons
-	public static final double BOTTOM_HEIGHT = 200;
+
 	public static final double BUTTON_WIDTH = 50;
 	public static final double BUTTON_HEIGHT = 35;
-	public static final double SCREEN_CHANGE_INTERVAL = 50;
 	
 	public static final double DRAG_SCROLL_THRESHOLD = 50;
 	public static final double SCROLL_VALUE_UNIT = 0.01;
