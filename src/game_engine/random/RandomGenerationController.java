0e71e2b2ec101cb3f2a46851324898edcf37ac68
ms548 <ms548@duke.edu>
2016-12-11 17:11:12 -0500

    implement random generation of clusters of sprites

--- src/game_engine/random/RandomGenerationController.java
+++ src/game_engine/random/RandomGenerationController.java
@@ -12,56 +12,28 @@ import game_object.level.Level;
 public class RandomGenerationController {
     private static final double OFFSET = 100;
     private Level myLevel;
-    private List<SpriteInfo> myRepeated;
+    private List<RandomSpriteCluster> myRepeated;
     private double myRepeatTime, myCurrentTime, myXRange, myYRange;
     
     public RandomGenerationController (Level level,
-                                       List<SpriteInfo> toRepeat,
-                                       double repeatTime) {
+                                       List<RandomSpriteCluster> toRepeat
+                                       ) {
         myLevel = level;
         myRepeated = toRepeat;
-        myRepeatTime = repeatTime;
-        myCurrentTime = 0;
-    }
-
-    public void setGenerationMetrics (double timeout, double xRange, double yRange) {
-        myXRange = xRange;
-        myYRange = yRange;
     }
 
     public void generateSprites (double elapsedTime) {
-        myCurrentTime += elapsedTime;
-        if (myCurrentTime >= myRepeatTime) {
-            myCurrentTime = 0;
-            generateSprites();
+        for(RandomSpriteCluster si : myRepeated){
+            if(si.shouldRender(elapsedTime)){
+                List<ISprite> sprites = si.getSprites();
+                for(ISprite s : sprites){
+                    double xPos = myLevel.getBoundary().right() + OFFSET;
+                    double yPos = 0;
+                    Position position = new Position(xPos, yPos);
+                    s.getPosition().addPosition(position);
+                    myLevel.addSprite(s);
+                }
+            }
         }
     }
-
-    private void generateSprites () {
-        for (SpriteInfo si : myRepeated) {
-            Class<? extends ISprite> c = si.getSpriteClass();
-            List<String> imagePaths = si.getImagePaths();
-            Dimension dim = si.getDimension();
-            try {
-                Constructor<? extends ISprite> ctor =
-                        c.getConstructor(Position.class, Dimension.class, List.class);
-                double xPos = new Random().nextDouble()*myXRange + myLevel.getBoundary().right() + OFFSET;
-                System.out.println(myLevel.getBoundary().getPosition().getX());
-                System.out.println("my level boundary right " + myLevel.getBoundary().right());
-                System.out.println(myLevel.getBoundary().getDimension().getWidth());
-                System.out.println("X POS " + xPos);
-                double yPos = new Random().nextDouble()*myYRange;
-                Position position = new Position(xPos, yPos);
-                ISprite sprite = ctor.newInstance(position, new Dimension(dim.getHeight(),dim.getWidth()), imagePaths);
-                
-                myLevel.addSprite(sprite);
-                
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
-
 }
--- src/game_engine/random/SpriteInfo.java
+++ src/game_engine/random/SpriteInfo.java
@@ -3,17 +3,20 @@ package game_engine.random;
 import java.util.List;
 import game_object.core.Dimension;
 import game_object.core.ISprite;
+import game_object.core.Position;
 
 public class SpriteInfo {
 
     private Class<? extends ISprite> myClass;
     private List<String> myImagePaths;
     private Dimension myDimension;
+    private Position myRelativePosition;
     
-    public SpriteInfo(Class<? extends ISprite> clas, List<String> imagePaths, Dimension dimension){
+    public SpriteInfo(Class<? extends ISprite> clas, List<String> imagePaths, Dimension dimension, Position position){
         myClass = clas;
         myImagePaths = imagePaths;
         myDimension = dimension;
+        myRelativePosition = position;
     }
     
     public Class<? extends ISprite> getSpriteClass() {
@@ -26,5 +29,9 @@ public class SpriteInfo {
     
     public Dimension getDimension() {
         return myDimension;
+    }
+    
+    public Position getRelativePosition() {
+        return myRelativePosition;
     }
 }
