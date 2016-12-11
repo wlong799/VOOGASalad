fbad16f59bb071f2d3e6fcf8a881a05d77ccc396
Bill Yu <han.yu@duke.edu>
2016-12-11 12:09:44 -0500

    add enemy difficulty

--- src/authoring/view/inspector/settings/CheckBoxView.java
+++ src/authoring/view/inspector/settings/CheckBoxView.java
@@ -5,32 +5,38 @@ import javafx.beans.value.ChangeListener;
 import javafx.scene.control.CheckBox;
 
 public class CheckBoxView extends AbstractSettingsView {
-    private String myTitle;
-    private boolean isSelected;
-    private ChangeListener<Boolean> myChangeListener;
+	private String myTitle;
+	private boolean isSelected;
+	private ChangeListener<Boolean> myChangeListener;
 
-    private CheckBox mySnapBox;
+	private CheckBox mySnapBox;
 
-    public CheckBoxView(AuthoringController controller, String title, boolean selected,
-                        ChangeListener<Boolean> changeListener) {
-        super(controller);
-        myTitle = title;
-        isSelected = selected;
-        myChangeListener = changeListener;
-    }
-    
-    @Override
-    public void initializeSettings() {
-        myLabel.setText(myTitle);
-        mySnapBox.selectedProperty().addListener(myChangeListener);
-        mySnapBox.setSelected(isSelected);
-    }
+	public CheckBoxView(AuthoringController controller, String title, boolean selected,
+			ChangeListener<Boolean> changeListener) {
+		super(controller);
+		myTitle = title;
+		isSelected = selected;
+		myChangeListener = changeListener;
+	}
 
-    @Override
-    protected void initUI() {
-        super.initUI();
-        mySnapBox = new CheckBox();
-        myContent.getChildren().add(mySnapBox);
-    }
-    
+	@Override
+	public void initializeSettings() {
+		myLabel.setText(myTitle);
+		mySnapBox.selectedProperty().addListener(myChangeListener);
+		mySnapBox.setSelected(isSelected);
+	}
+
+	@Override
+	protected void initUI() {
+		super.initUI();
+		mySnapBox = new CheckBox();
+		myContent.getChildren().add(mySnapBox);
+	}
+
+	@Override
+	protected void updateLayoutSelf() {
+		super.updateLayoutSelf();
+		mySnapBox.setPrefWidth(this.getWidth());
+	}
+
 }
