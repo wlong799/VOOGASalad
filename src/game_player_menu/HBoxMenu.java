46eb379fe3e1bd47833fe5b3ed67d5a711fe5632
Sam Curtis <sjc43@duke.edu>
2016-12-10 21:41:39 -0500

    Added scrollpane for description, moved strings to resource files, added load game button

--- src/game_player_menu/HBoxMenu.java
+++ src/game_player_menu/HBoxMenu.java
@@ -14,10 +14,12 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class HBoxMenu extends MenuSceneGenerator{
 	private ResourceBundle myResources;
+	private HBox myItemsBox;
 	
 	public HBoxMenu(IMenuInputListener menuListener, Stage s) {
 		super(menuListener,s);
@@ -26,13 +28,32 @@ public class HBoxMenu extends MenuSceneGenerator{
 
 	@Override
 	protected Node layoutDescriptions(List<DisplayableItemDescription> menuItems) {
-		HBox itemsBox = new HBox();
-		itemsBox.setAlignment(Pos.CENTER);
-		itemsBox.setSpacing(50);
+		myItemsBox = new HBox();
+		Double paneWidth = getMaxItemWidth(menuItems);
+		myItemsBox.setAlignment(Pos.CENTER);
+		myItemsBox.setSpacing(Double.parseDouble(myResources.getString("ItemSpacing")));
 		for(DisplayableItemDescription currItem : menuItems){
-			itemsBox.getChildren().add(currItem.getPaneRepresentation());
+			Pane itemPane = currItem.getPaneRepresentation();
+			itemPane.setPrefWidth(paneWidth);
+			myItemsBox.getChildren().add(currItem.getPaneRepresentation());
 		}
-		return itemsBox;
+		return myItemsBox;
+	}
+	
+	private Double getMaxItemWidth(List<DisplayableItemDescription> menuItems){
+		Double maxWidth = Double.MIN_VALUE;
+		for(DisplayableItemDescription item : menuItems){
+			Double width = item.getPaneRepresentation().getMaxWidth();
+			if(width > maxWidth){
+				maxWidth = width;
+			}
+		}
+		return maxWidth;
+	}
+
+	@Override
+	protected void displayNewItem(DisplayableItemDescription item) {
+		myItemsBox.getChildren().add(item.getPaneRepresentation());
 	}
 
 
