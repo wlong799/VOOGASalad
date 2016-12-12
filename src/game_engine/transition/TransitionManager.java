9657e421782ff84a0660e9ee48ab9e3923edcfa9
Grant Costa <gac19@duke.edu>
2016-12-12 04:42:23 -0500

    Sad face end screen with buttons TODO

--- src/game_engine/transition/TransitionManager.java
+++ src/game_engine/transition/TransitionManager.java
@@ -2,9 +2,11 @@ package game_engine.transition;
 
 import game_object.core.Game;
 import game_object.level.Level;
+import game_player.GamePlayer;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -20,6 +22,9 @@ public class TransitionManager extends AbstractTransitionManager {
     
         public static final String GAME_OVER = "GAME OVER!!!";
         public static final String FROWN = "img/frown.png";
+        public static final String REPLAY = "Replay";
+        public static final String QUIT = "QUIT";
+        public static final int SPACING = 50;
 
 	private Level myLevel;
 
@@ -60,11 +65,30 @@ public class TransitionManager extends AbstractTransitionManager {
 	private void showGameOver(){
             Stage s = new Stage();
             VBox root = new VBox();
+            root.setSpacing(SPACING);
+            root.setMaxHeight(Integer.MAX_VALUE);
             Label gameOver = new Label(GAME_OVER);
-            gameOver.setFont(new Font(50));
-            root.getChildren().addAll(gameOver,new ImageView(FROWN));
+            gameOver.setFont(new Font(SPACING));
+            HBox buttons = new HBox();
+            buttons.setMaxWidth(Integer.MAX_VALUE);
+            Button replay = new Button(REPLAY);
+            Button quit = new Button(QUIT);
+            replay.setPrefWidth(myGame.getScreenSize().getWidth()/4);
+            quit.setPrefWidth(myGame.getScreenSize().getWidth()/4);
+            buttons.setSpacing(SPACING);
+            replay.setOnAction((event)->{
+                //TODO
+                //myGame.setFirstSceneAsLevel(myLevel);
+                //GamePlayer g = new GamePlayer(new Stage(),myGame);
+            });
+            quit.setOnAction((event) -> {
+                //TODO
+            });
+            buttons.getChildren().addAll(replay,quit);
+            buttons.setAlignment(Pos.CENTER);
+            root.getChildren().addAll(gameOver,new ImageView(FROWN),buttons);
             root.setAlignment(Pos.CENTER);
-            s.setScene(new Scene(root,400,400));
+            s.setScene(new Scene(root,myGame.getScreenSize().getWidth()/2,myGame.getScreenSize().getHeight()/2));
             s.show();
 	}
 }
